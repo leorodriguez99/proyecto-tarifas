d66d3a32afcdebc7a81cfa63fdcae9b132435c27
leorodriguez99 <leorodriguez8899@gmail.com>
2025-07-15 00:07:22 -0300

    correcciones

--- prueba-tres/vite.config.js
+++ prueba-tres/vite.config.js
@@ -2,7 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [tailwindcss(), react()],
+  server: {
+    // Esto ya expone el servidor en tu red local
+    host: true, 
+
+    // Agregá esta sección para el proxy
+    proxy: {
+      // Cualquier petición que empiece con /api...
+      '/api': {
+        // ...será redirigida a tu backend de NestJS
+        target: 'http://localhost:3001',
+        
+        // Necesario para que el backend acepte la petición
+        changeOrigin: true,
+
+        // Elimina /api de la ruta antes de enviarla al backend
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
